3a61478cb3d62b6be628008d734a4898968cba16
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-05-15 01:09:52 +0600

    added data base post and server get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express()
 const port =process.env.PORT || 3000;
@@ -29,6 +29,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection =client.db("coffeesDB").collection("coffees")
+    
+    app.post('/coffees',async(req,res)=>{
+      const newCoffee =req.body;
+      const result =await userCollection.insertOne(newCoffee);
+      res.send(result)
+    })
+
+    app.delete('/coffees/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result =await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/coffees',async(req,res)=>{
+      const result=await userCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/coffees/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result =await userCollection.findOne(query)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
